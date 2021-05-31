c4208ff3f590206eeb26140c188b2a5e38adde87
Andrey Kolomoets <kolomoets.and@gmail.com>
2021-05-31 16:55:12 +0300

    index

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// Hmaburger & NavbarMobile
 $(document).ready(function() {
 	$('.hamburger').click(function() {
 
@@ -48,8 +47,40 @@ $(document).ready(function() {
         $(this).toggleClass('open');
     });
 
+    var sliderWidth = $('.wrapper_right_bar').width();
+    $('.index_slide_block').css("width", sliderWidth);
+
+    $('.index_slider_wrapper').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        variableWidth: true,
+        slidesToShow: 1,
+        arrows: false,
+        appendDots: $('.slick-slider-dots'),
+        adaptiveHeight: true
+    });
+
+    var allSlideLength = $('.index_slider_wrapper .index_slide_block').length;
+    var clonedSlider = $('.index_slider_wrapper .slick-cloned').length;
+    var sliderLength = allSlideLength - clonedSlider;
+
+    if (sliderLength.length > 1) {
+        $('.last_slide').text(sliderLength);
+        $('.index_slider_wrapper').on('afterChange', function(event, slick, currentSlide) {
+            $('.first_slide').text(currentSlide + 1);
+        });
+    } else {
+        $('.last_slide').text('0' + sliderLength);
+        $('.index_slider_wrapper').on('afterChange', function(event, slick, currentSlide) {
+            $('.first_slide').text('0' + (currentSlide + 1));
+        });
+    };
+
     $('.popular_slider').slick({
         dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
         infinite: true,
         speed: 300,
         variableWidth: true,
@@ -60,6 +91,8 @@ $(document).ready(function() {
 
     $('.bestsellers_slider').slick({
         dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
         infinite: true,
         speed: 300,
         variableWidth: true,
@@ -70,6 +103,8 @@ $(document).ready(function() {
 
     $('.materials_slider').slick({
         dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
         infinite: true,
         speed: 300,
         variableWidth: true,
@@ -82,6 +117,8 @@ $(document).ready(function() {
 
     $('.blog_slider').slick({
         dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
         infinite: true,
         speed: 300,
         variableWidth: true,
